7a83f28e480d2e3604b086b1619b7d50a1d4ba9f
Arshad <2arshu@gmail.com>
2020-08-12 07:09:35 +0530

    Initial commit

---
+++ RWPickFlavor/RWPrint.swift
@@ -0,0 +1,17 @@
+//
+//  RWPrint.swift
+//  RWPickFlavor
+//
+//  Created by Arshad on 12/08/20.
+//  Copyright © 2020 Arshad. All rights reserved.
+//
+
+import Foundation
+
+public class RWPrint  {
+  
+    public class func rwPrint(message:String) {
+        print(message)
+    }
+    
+}
